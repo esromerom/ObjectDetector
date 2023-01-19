e3736e73b94b05f5721c44abea57e3689498f988
ROMERO, Erik <esromerom@gmail.com>
2023-01-20 00:31:36 +0100

    15 - Added basic class definition.
    Added contructor destructor and some simple methods, struct and attributes of the class.

---
+++ src/cpp/config_handler/configHandler.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * @file       configHandler.cpp
+ * @copyright  Semillero SIRA 2022 -
+ *             Universidad de la Salle, Bogota, Colombia
+ *
+ * @brief      Class definition file to handle the settings that govern the program
+ ******************************************************************************/
+
+#include <string>
+
+/**
+ * @struct  NoiseFilterConfig
+ * @brief   Stores the parameters to be used for Noise filtering, including
+ *          the kind of filter to use.
+ */
+struct NoiseFilterConfig {
+    double mean_k;
+    double std_dev_mul_thresh;
+};
+
+/**
+ * @class   ConfigHandler
+ * @brief   Handles the configuration object. I may allow to have several instances of
+ *          configuration on one execution. Even to modify or reload config from
+ *          JSON files.
+ * @note    Needs JsonHandler class.
+ */
+class ConfigHandler {
+public:
+    ConfigHandler();
+    ~ConfigHandler();
+    void loadConfig(const std::string& file_name);
+    NoiseFilterConfig getNoiseFilterConfig();
+    void setNoiseFilterConfig(const NoiseFilterConfig& config);
+
+private:
+    nlohmann::json json_;
+    NoiseFilterConfig noise_filter_config_;
+};
